dfbf4a0404dd11fe3ffa43eeacd495d8fec818b5
Ben Sonley <lbsonley@gmail.com>
2020-06-04 10:38:05 +0200

    init ✨

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node Module Template
+
+**_Boilerplate tooling for developing radical node modules_**
---
+++ lib/components/component.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Comp = () => <h1>Component</h1>;
+
+export default Comp;
